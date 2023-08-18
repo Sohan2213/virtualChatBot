c87b4870af609f55c53decbb97a113160b0e4d57
Avinash <avinash31803@gmail.com>
2023-08-19 03:27:55 +0530

    added functionality to recording button

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -5,19 +5,21 @@ import RecordMessage from './recordMessage';
 export default function  () {
     const [isLoading,setIsLoading] = useState(false); 
     const[messages,setMessages] = useState<any[]>([]);
-
-    const createBlogUrl = (data:any)=>{
-
+    // const [blob,setBlob] = useState("");
+    const createBlobUrl = (data:any)=>{
+      
     };
     // when the user stops the recording
-    const handleStop = async () =>{
-
+    const handleStop = async (blobUrl:string) =>{
+      console.log(blobUrl);
+      // setBlob(blobUrl);
     };
   return (
     <div>
       <div className='h-screen overflow-y-hidden'>
         <Title setMessages={setMessages}/>
         <div className='flex flex-col justify-between h-full overflow-y-scroll pb-96'>
+            {/* <audio src={blob} controls /> */}
             {/* Recorder */}
             <div className='fixed bottom-0 w-full border-t text-center p-2 bg-gradient-to-r from-sky-500 to-violet-500 '>
               <div className='flex justify-center items-center w-full'>
--- frontend/src/components/recordMessage.tsx
+++ frontend/src/components/recordMessage.tsx
@@ -7,14 +7,15 @@ type Props = {
 }
 
 function recordMessage({ handleStop }: Props) {
-  return <ReactMediaRecorder 
-    audio
-    onStop={handleStop}
-    render={({ status, startRecording, stopRecording})=>(<div className="mt-2">
-        <button className="bg-white p-4 rounded-full" onMouseDown={startRecording} onMouseUp={stopRecording}>Icon</button>
-        <p className="mt-2 text-white font-light">{status}</p>
+    return <ReactMediaRecorder
+        audio
+        onStop={handleStop}
+        render={({ status, startRecording, stopRecording }) => (<div className="mt-2">
+            <button className="bg-white p-4 rounded-full" onMouseDown={startRecording} onMouseUp={stopRecording}><RecorderIcon classText={status == "recording"?"animate-pulse text-red-500":"text-sky-500"}/></button>
+            <p className="mt-2 text-white font-light">{status}</p>
         </div>)}
-  />
+
+    />
 }
 
 export default recordMessage
