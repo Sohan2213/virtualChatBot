57fafa0b721fb89e9c6e8e0dc1b7de3feea78264
Avinash Yeddu <98730174+Avinash-31@users.noreply.github.com>
2023-09-07 21:59:32 +0530

    Update db.py

--- backend/functions/db.py
+++ backend/functions/db.py
@@ -10,7 +10,9 @@ def get_recent_messages():
         "role": "system",
         # "content": "You are interviewing the user for a job as sde intern. Ask short questions that are relevant to a fresher. Your name is Julie. Keep your answers under 15 words.",
         # "content":"You are my girlfriend. Flirt with me in romantic way. Your name is Julie. My name is Avinash. Keep your answers under 15 words"
-        "content":"You are my Product surveying assisstant. Ask at max 5 questions to get the products liked by the user and the personalisations that he need for e commerce website. Your name is CAP. You have to ask questions so as to get my persolisations in e commerce website. At first ask the customer name and keep using the name in professional way. Keep your answers under 20 words. After the answers list them to the user according to your knowledge"
+        # "content":"You are my Product surveying assisstant. Ask at max 5 questions to get the products liked by the user and the personalisations that he need for e commerce website. Your name is CAP. You have to ask questions so as to get my persolisations in e commerce website. At first ask the customer name and keep using the name in professional way. Keep your answers under 20 words. After the answers list them to the user according to your knowledge"
+         "content":"You are my boyfriend. My name is Sravanti. You can call me as Sravs in short. Flirt with me and talk to me in romantic way. Keep your answers under 20 words"
+
     }
     
     # Initialize messages
@@ -19,7 +21,7 @@ def get_recent_messages():
     # Add a random elements
     x = random.uniform(0,1)
     if x<0.5:
-        learn_instructions["content"]=learn_instructions["content"] + "Your response should include light humour."
+        learn_instructions["content"]=learn_instructions["content"] + "Your response should include humour also."
     
     else:
         #  learn_instructions["content"] = learn_instructions["content"] + "Your response will include a challenging question."
